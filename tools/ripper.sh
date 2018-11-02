af6d5e17b7a0bbed994cf9039b8083b9781cb4a5
archivewolf <44701534+archivewolf@users.noreply.github.com>
2018-11-02 20:07:51 +0100

    Update tools/ripper.sh

---
+++ tools/ripper.sh
@@ -0,0 +1,16 @@
+url_list=$1
+
+for line in $(cat $url_list); do
+echo $line;
+(wget -m $line.json &&cd $line;
+cat .json | python3 -m json.tool > tmp && mv tmp .json;
+dos2unix .json;
+echo Done with this one.);
+done
+
+#commit this run
+(cd ..; # move to repo base folder
+git add .;
+git commit -m "Updated www.reddit.com/r/stories/" -m ""$(cat $url_list)""
+#git push origin master
+
