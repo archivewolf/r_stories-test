cea8cc688745d9c8853ed9dcdfe0429adffd44b6
archivewolf <44701534+archivewolf@users.noreply.github.com>
2018-11-02 22:34:36 +0100

    Fixed wget command

--- tools/ripper.sh
+++ tools/ripper.sh
@@ -5,7 +5,9 @@ for line in $(cat $url_list); do
   echo '['$timestamp']' Working on $line;
   (cd ..; #change to repo base
   echo '['$timestamp']' Getting json file...;
-  wget -m $line.json && cd $line;
+  wget -m $line.json;
+  echo '['$timestamp']' Got file...;
+  cd $line;
   echo '['$timestamp']' Changed to dir: $(pwd);
   echo '['$timestamp']' Beautify json...;
   cat .json | python3 -m json.tool > tmp && mv tmp .json;
